91d0d87314518b85dfd95c7d9a24212032b014cd
Dave Rensberger <drensber@gmail.com>
2012-04-30 00:15:09 -0400

    helps to know exactly how cp works!

--- mpbox_buildroot/post-build-script.sh
+++ mpbox_buildroot/post-build-script.sh
@@ -5,7 +5,7 @@ TARGET_ROOTFS=$1
 MPBOX_TOPDIR=`pwd`/..
 
 
-#echo "Running post-build-script.  TARGET_ROOTFS=${TARGET_ROOTFS} MYDIR=${MYDIR}"
-
-cp -r ${MPBOX_TOPDIR}/mpbox_buildroot/fs/rootfs_customization/* ${TARGET_ROOTFS}
+echo "Running post-build-script."
+echo "cp -rf --remove-destination ${MPBOX_TOPDIR}/mpbox_buildroot/fs/rootfs_customization/* ${TARGET_ROOTFS}"
+cp -rf --remove-destination ${MPBOX_TOPDIR}/mpbox_buildroot/fs/rootfs_customization/* ${TARGET_ROOTFS}
 
