07b043c6e34629b17ea5b712f6c993b6b8c2c339
Dave Rensberger <drensber@gmail.com>
2012-05-04 01:13:03 -0400

    Making mpservice install into main image

--- mpbox_buildroot/post-build-script.sh
+++ mpbox_buildroot/post-build-script.sh
@@ -1,11 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-TARGET_ROOTFS=$1
+TARGET_ROOTFS=${1}
 
-MPBOX_TOPDIR=`pwd`/..
+MPBOX_TOPDIR=`(cd ..; pwd)`
 
-
+cd ${MPBOX_TOPDIR}
+. ./x86-uClibc-linux.env
+cd -
+ 
+echo
 echo "Running post-build-script."
+echo
+echo "Copying rootfs_customization:"
 echo "cp -rf --remove-destination ${MPBOX_TOPDIR}/mpbox_buildroot/fs/rootfs_customization/* ${TARGET_ROOTFS}"
 cp -rf --remove-destination ${MPBOX_TOPDIR}/mpbox_buildroot/fs/rootfs_customization/* ${TARGET_ROOTFS}
 
+echo
+echo "Building mpservice code:"
+make -C ${MPBOX_TOPDIR}/mpservice 
+
+echo "Installing mpservice code:"
+make -C ${MPBOX_TOPDIR}/mpservice install \
+    MPSERVICE_BIN_DIR=${TARGET_ROOTFS}/mpservice/bin \
+    MPSERVICE_WWW_DIR=${TARGET_ROOTFS}/mpservice/www \
+    MPSERVICE_CGI_DIR=${TARGET_ROOTFS}/mpservice/cgi-bin \
+    MPSERVICE_LIB_DIR=${TARGET_ROOTFS}/lib \
+    MPSERVICE_MISC_DIR=${TARGET_ROOTFS}/mpservice/misc \
+    STRIP=${CROSS_COMPILE}strip
